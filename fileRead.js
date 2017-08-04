e119321abcd30d8b27aa6be35680df45514da1a1
tausiftt5238 <tausiftt5238@gmail.com>
2017-08-04 19:30:22 +0600

    first commit

---
+++ fileRead.js
@@ -0,0 +1,22 @@
+/**
+ * Created by tausiftt5238 on 8/4/17.
+ */
+var sentences = [];
+function LoadFile() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        //console.log(xhr.responseText);
+        if (this.readyState == 4 && this.status == 200) {
+            sentences = xhr.responseText.split("\n");
+            document.getElementById("showText").innerHTML =
+                sentences[0];
+        }
+    }
+    xhr.open("GET","bangla.txt");
+    xhr.send();
+}
+
+function loadText(element, index){
+    document.getElementById(element).innerHTML =
+        sentences[index];
+}
