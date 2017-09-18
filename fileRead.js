379c8f51b5fe60eac2ae61ba8c4093c3785f745f
tausiftt5238 <tausiftt5238@gmail.com>
2017-09-19 01:53:24 +0600

    updated

--- fileRead.js
+++ fileRead.js
@@ -8,7 +8,7 @@ function LoadFile() {
         //console.log(xhr.responseText);
         if (this.readyState == 4 && this.status == 200) {
             sentences = xhr.responseText.split("\n");
-            index = (parseInt(Math.random()*100)) % 100;
+            index = (parseInt(Math.random()*102)) % 102;
             document.getElementById("showText").innerHTML =
                 sentences[index];
         }
