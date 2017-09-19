ed22a5652922bffe635d0a67123ca9ec9461341b
tausiftt5238 <tausiftt5238@gmail.com>
2017-09-19 17:17:01 +0600

    Login/signup

--- app.js
+++ app.js
@@ -25,4 +25,22 @@
         const promise = auth.signInWithEmailAndPassword(email,pass);
         promise.catch(e => console.log(e.message));
     });
+
+    //Add signup event
+    btnSignUp.addEventListener('click' , e => {
+        const email =txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        //Sign in
+        const promise = auth.createUserWithEmailAndPassword(email,pass);
+        promise.catch(e => console.log(e.message));
+    });
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+       if(firebaseUser){
+           console.log(firebaseUser);
+       }else{
+           console.log('not logged in');
+       }
+    });
 }());
